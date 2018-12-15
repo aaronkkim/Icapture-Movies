fa027b6c3fc53ae7e7bd19ba5c2baad8968b1d52
aaronkkim <aaron.molly.kim@gmail.com>
2018-12-15 13:22:53 -0700

    Took a break for lunch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import MovieRow from './movieRow.js'
+import $ from 'jquery'
 
 class App extends Component {
   constructor(props){
     super(props)
-    console.log("This is my initializer")
-    const movies = [
-      {poster_src:"https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/t/5aabd60603ce641d203135e6/1521210900998/epic-poster-released-for-avengers-infinity-war-packs-in-a-ton-of-characters1?format=1500w", id:0, title:"Avengers 1",overview:"marvel movie"},
-      {poster_src:"https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/t/5aabd60603ce641d203135e6/1521210900998/epic-poster-released-for-avengers-infinity-war-packs-in-a-ton-of-characters1?format=1500w",id:1, title:"Avengers 2",overview:"2marvel 2movie"}
-    ]
+    this.state ={}
+   // console.log("This is my initializer")
+  //   const movies = [
+  //     {poster_src:"https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/t/5aabd60603ce641d203135e6/1521210900998/epic-poster-released-for-avengers-infinity-war-packs-in-a-ton-of-characters1?format=1500w", id:0, title:"Avengers 1",overview:"marvel movie"},
+  //     {poster_src:"https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/t/5aabd60603ce641d203135e6/1521210900998/epic-poster-released-for-avengers-infinity-war-packs-in-a-ton-of-characters1?format=1500w",id:1, title:"Avengers 2",overview:"2marvel 2movie"}
+  //   ]
 
-  var movieRows =[]
-  movies.forEach((movie)=>{
-    console.log (movie.title)
-    const movieRow = <MovieRow movie ={movie}/>
+  // var movieRows =[]
+  // movies.forEach((movie)=>{
+  //   console.log (movie.title)
+  //   const movieRow = <MovieRow movie ={movie}/>
  
-    movieRows.push(movieRow)
-  })
-  this.state ={rows:movieRows}
+  //   movieRows.push(movieRow)
+  // })
+  // this.state ={rows:movieRows}
+  this.performSearch()
+  }
+  performSearch(){
+    console.log("Perform search using movieDb")
   }
   render() {
     return (
